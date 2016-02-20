62551ffa373ec3ec7810c72d8d2bddcc7a418115
prp90 <p.prado90@gmail.com>
2016-02-20 09:53:56 +0100

    Add buttons

--- app/src/main/java/pp222du_am222sq/projectapp/MainActivity.java
+++ app/src/main/java/pp222du_am222sq/projectapp/MainActivity.java
@@ -2,15 +2,44 @@ package pp222du_am222sq.projectapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button on_button, off_button, nothing_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        on_button = (Button) findViewById(R.id.turn_on);
+        off_button = (Button) findViewById(R.id.turn_off);
+        nothing_button = (Button) findViewById(R.id.do_nothing);
+
+        on_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("debug", "TURN ON pressed");
+            }
+        });
+
+        off_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("debug", "TURN OFF pressed");
+            }
+        });
+
+        nothing_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("debug", "DO NOTHING pressed");
+            }
+        });
     }
 
     @Override
