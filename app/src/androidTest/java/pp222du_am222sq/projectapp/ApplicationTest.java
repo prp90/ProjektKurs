39503f0a2d75a75025968d0ece1e515edf586a04
prp90 <p.prado90@gmail.com>
2016-02-20 09:32:12 +0100

    Initial commit

---
+++ app/src/androidTest/java/pp222du_am222sq/projectapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pp222du_am222sq.projectapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
