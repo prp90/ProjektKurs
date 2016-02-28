454d65f89730f993a476b82304367b9e10fae9af
Priscila Prado <Priscila Prado>
2016-02-28 13:06:50 +0100

    Send token to server

--- app/src/main/java/pp222du_am222sq/projectapp/RegistrationIntentService.java
+++ app/src/main/java/pp222du_am222sq/projectapp/RegistrationIntentService.java
@@ -11,7 +11,17 @@ import com.google.android.gms.gcm.GcmPubSub;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Created by Priscila on 2016-02-24.
@@ -19,6 +29,7 @@ import java.io.IOException;
 public class RegistrationIntentService extends IntentService {
 
     private static final String TAG = "RegIntentService";
+    private Socket socket;
     private static final String[] TOPICS = {"global"};
 
     public RegistrationIntentService() {
@@ -46,6 +57,7 @@ public class RegistrationIntentService extends IntentService {
             // TODO: Implement this method to send any registration to your app's servers.
             sendRegistrationToServer(token);
 
+            Log.i(TAG, "SENT");
             // Subscribe to topic channels
             subscribeTopics(token);
 
@@ -74,7 +86,24 @@ public class RegistrationIntentService extends IntentService {
      * @param token The new token.
      */
     private void sendRegistrationToServer(String token) {
-        // Add custom implementation, as needed.
+
+        try {
+            InetAddress serverAddr = InetAddress.getByName("77.105.197.96");
+
+            socket = new Socket(serverAddr, 8383);
+
+            PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())),
+                    true);
+            out.println(token);
+
+        } catch (UnknownHostException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+
     }
 
     /**
